a59275220aa7955330328ebdcf9abebecbda52e4
Sebastian Matkovich <e1026902@student.tuwien.ac.at>
2018-06-11 19:49:39 +0200

    final for today

--- grueneis1/orbit.f90
+++ grueneis1/orbit.f90
@@ -1,7 +1,8 @@
 PROGRAM ORBIT
    USE constants
    IMPLICIT NONE
-
+   
+   !real :: force_sun
 !  Define position and velocity Vector/Arrays
    REAL(KIND=p), DIMENSION(2) :: r,v
 
@@ -14,9 +15,9 @@ PROGRAM ORBIT
 
 !  Define simulation length and time step size first
 
-   WRITE(*,*)'#Welcome to Orbit calculator!'
-!   WRITE(*,*)'#Time step is set to:       ',dt,' days.'
-!   WRITE(*,*)'#Number of steps is set to: ',MAXN,'.'
+   WRITE(*,*)'!Welcome to Orbit calculator!'
+!   WRITE(*,*)'!Time step is set to:       ',dt,' days.'
+!   WRITE(*,*)'!Number of steps is set to: ',MAXN,'.'
 
 !  Define initial paramters
    t=0
@@ -25,17 +26,17 @@ PROGRAM ORBIT
    OPEN(unit=11,file="EarthOrbit_Euler.dat")
    OPEN(unit=12,file="EarthOrbit_Energy_Euler.dat")
    WRITE(*,*)' '
-   WRITE(*,*)'#Calculating EarthOrbit using first order Euler method.'
-   WRITE(*,*)'#Results will be written to EarthOrbit_Euler.dat and EarthOrbit_Energy_Euler.dat files.'
-!   DO N=1,MAXN
+   WRITE(*,*)'!Calculating EarthOrbit using first order Euler method.'
+   WRITE(*,*)'!Results will be written to EarthOrbit_Euler.dat and EarthOrbit_Energy_Euler.dat files.'
+   DO N=1,MAXN
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!  IMPLEMENT EULER INTEGRATION HERE  !!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
-      r(N) = r(N-1) + v(N-1)
-      v(N) = v(N-1) - 13.3159162*333054.253*r(N-1)/pow(fabs(r(N-1)), 3)
+      r(N, N) = r(N-1, N-1) + v(N-1, N-1)
+      v(N, N) = v(N-1, N-1) - 13.3159162*333054.253*r(N-1, N-1)/pow(norm2(r(N-1, N-1)), 3)
       WRITE(11,*)t, r
-      WRITE(12,*)t, ENERGY(Me,r,v)
+!      WRITE(12,*)t, ENERGY(Me,r,v)
 
 !   ENDDO
 
@@ -48,10 +49,10 @@ PROGRAM ORBIT
    CALL INIT_EARTH(r,v)
 
    OPEN(unit=11,file="EarthOrbit_RK4.dat")
-   OPEN(unit=12,file="EarthOrbit_Energy_RK4.dat")
+!   OPEN(unit=12,file="EarthOrbit_Energy_RK4.dat")
    WRITE(*,*)' '
-   WRITE(*,*)'#Calculating EarthOrbit using RK4 method.'
-   WRITE(*,*)'#Results will be written to EarthOrbit_RK4.dat and EarthOrbit_Energy_RK4.dat files.'
+   WRITE(*,*)'!Calculating EarthOrbit using RK4 method.'
+   WRITE(*,*)'!Results will be written to EarthOrbit_RK4.dat and EarthOrbit_Energy_RK4.dat files.'
 
 !   DO N=1,MAXN
      
@@ -61,7 +62,7 @@ PROGRAM ORBIT
 !     Integrate r and v using RK4
 
       WRITE(11,*)t, r
-      WRITE(12,*)t, ENERGY(Me,r,v)
+!      WRITE(12,*)t, ENERGY(Me,r,v)
 !   ENDDO
    
    CLOSE(11)
@@ -70,40 +71,40 @@ PROGRAM ORBIT
 
 
 
-CONTAINS
+!CONTAINS
 
 !                                                                                           ->
 ! This function returns the gravitational force of the sun acting on a mass m at coordinate  r  
 !
 
-FUNCTION FORCE_SUN(m,r)
-   USE constants
-   IMPLICIT NONE
-   REAL(KIND=p), DIMENSION(2) :: FORCE_SUN
-   REAL(KIND=p), DIMENSION(2) :: r      ! position of mass
-   REAL(KIND=p) :: m                    ! and mass m
+!real function FORCE_SUN(m,r)
+!   USE constants
+!   IMPLICIT NONE
+!   REAL(KIND=p), DIMENSION(2) :: FORCE_SUN
+!   REAL(KIND=p), DIMENSION(2) :: r      ! position of mass
+!   REAL(KIND=p) :: m                    ! and mass m
 
-   RETURN
-END
+ !  FORCE_SUN
+!END
 
 
 !                                                                                  ->
 ! This function returns the kinetic and potential energy of a mass m at coordinate  r  in the grav. field of the sun
 !
 
-FUNCTION ENERGY(m,r,v)
-   USE constants
-   IMPLICIT NONE
-   REAL(KIND=p) :: ENERGY
-   REAL(KIND=p) :: m
-   REAL(KIND=p), DIMENSION(2) :: r,v
+!FUNCTION ENERGY(m,r,v)
+ !  USE constants
+  ! IMPLICIT NONE
+   !REAL(KIND=p) :: ENERGY
+   !REAL(KIND=p) :: m
+   !REAL(KIND=p), DIMENSION(2) :: r,v
 
 !
 !   GESAMTENERGIE MUSS HIER RICHTIG BERECHNET WERDEN.
 !
 
-   RETURN
-END
+  ! RETURN
+!END
 
 !                                 ->    ->
 ! This routine defines the initial r and v values for the earth.
